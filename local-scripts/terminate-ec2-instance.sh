5f48ae14b59f717ed5c0dce689fe26d70fc378a2
Valerio Ferretti <valerio.ferretti92@gmail.com>
2020-08-23 20:38:39 +0200

    Bug fix
    Filtering instances by state equal to running in termination script

--- local-scripts/terminate-ec2-instance.sh
+++ local-scripts/terminate-ec2-instance.sh
@@ -2,7 +2,8 @@
 set -e
 
 INSTANCE_ID=$(aws ec2 describe-instances \
-    --filters "Name=tag:name,Values=my0p3nvpn-instance" | jq -r '.Reservations[0].Instances[0].InstanceId')
+    --filters "Name=tag:name,Values=my0p3nvpn-instance" \
+    --filters "Name=instance-state-name,Values=running" | jq -r '.Reservations[0].Instances[0].InstanceId')
 if [ -n "$INSTANCE_ID" ];
 then
     aws ec2 terminate-instances --instance-ids $INSTANCE_ID | cat
